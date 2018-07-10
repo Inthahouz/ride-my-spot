226c1d94015f4aabce1ea7e1c1a66e15013f4b41
Nicolas Bonnamour <nicolas@8oclock.fr>
2018-07-10 14:11:58 +0200

    first page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public platform: Platform, public navCtrl: NavController) {
 
+  }
+
+  redirectGoogle() {
+    this.platform.ready().then(() => {
+       new InAppBrowser("https://www.google.com",'_blank');
+  });
   }
 
 }
